18e4aba0d4cf28ccb892dc8dc2e4794746f1bc67
zzzpotato <35818234+zzzpotato@users.noreply.github.com>
2018-10-23 11:20:14 -0500

    Windows build fix

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#Run with NO_PROTON=1 ./zcutil/build-win.sh --disable-rust
+
 HOST=x86_64-w64-mingw32
 CXX=x86_64-w64-mingw32-g++-posix
 CC=x86_64-w64-mingw32-gcc-posix
@@ -9,20 +12,21 @@ set -eu -o pipefail
 
 # If --disable-rust is the next argument, disable Rust code:
 RUST_ARG=''
+RUST_FLAG=''
 if [ "x${1:-}" = 'x--disable-rust' ]
 then
     RUST_ARG='--enable-rust=no'
+    RUST_FLAG=1
     shift
 fi
 
 set -x
-cd "$(dirname "$(readlink -f "$0")")/.."
+cd "$(dirname "$(readlink -f "$0")")/.." 
+cd depends/ && make HOST=$HOST V=1 NO_QT=1 NO_RUST="$RUST_FLAG"
+cd "$(dirname "$(readlink -f "$0")")/.." 
 
-
-cd depends/ && make HOST=$HOST V=1 NO_QT=1 NO_RUST="$RUST_ARG" && cd ../
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS+=" -fopenmp" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-zmq --disable-rust "$RUST_ARG"
 sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
 cd src/
 CC="${CC}" CXX="${CXX}" make V=1 zerod.exe zero-cli.exe zero-tx.exe
-
