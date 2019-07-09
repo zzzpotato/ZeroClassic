3cb6c5fca89342ce1ea4ecbfe9a8f1e1c59181df
Potato <35818234+zzzpotato@users.noreply.github.com>
2019-07-09 14:33:25 -0500

    Fix Deprecation height

--- src/deprecation.h
+++ src/deprecation.h
@@ -10,10 +10,10 @@
 // * A warning is shown during the 4 weeks' worth of blocks prior to shut down.
 static const int APPROX_RELEASE_HEIGHT = 620000;
 static const int WEEKS_UNTIL_DEPRECATION = 52;
-static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 60);
+static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 30);
 
 // Number of blocks before deprecation to warn users
-static const int DEPRECATION_WARN_LIMIT = 28 * 24 * 60; // 4 weeks
+static const int DEPRECATION_WARN_LIMIT = 28 * 24 * 30; // 4 weeks
 
 /**
  * Checks whether the node is deprecated based on the current block height, and
