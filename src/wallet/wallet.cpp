6f04e3900b8bd240de535d95811a411f6e18cc2f
miodragpop <miodragpopovic@stcable.net>
2020-02-17 17:20:29 +0100

    force freed memory to return back to the OS

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -35,6 +35,8 @@
 #include <boost/filesystem.hpp>
 #include <boost/thread.hpp>
 
+#include <malloc.h>
+
 using namespace std;
 using namespace libzcash;
 
@@ -3167,6 +3169,9 @@ void CWallet::DeleteTransactions(std::vector<uint256> &removeTxs)
             return;
         }
     }
+    
+    // Miodrag: release memory back to the OS
+    malloc_trim(0);
 }
 
 void CWallet::DeleteWalletTransactions(const CBlockIndex* pindex)
